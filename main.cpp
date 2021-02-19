1d290d0d1ba8226cfe23ab4ca057866b72b377d0
OPS 400 <79289232+ops400@users.noreply.github.com>
2021-02-19 10:27:48 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	a = 1;
+	
+	while (a = 1)
+	{	
+		cout << "\033[41m \033[0m";
+		cout << "\033[42m \033[0m";
+		cout << "\033[43m \033[0m";
+		cout << "\033[44m \033[0m";
+		cout << "\033[45m \033[0m";
+		cout << "\033[0m \n";
+	}
+
+	/* this is just a simple rainbow code which uses a while loop to draw some colors */
+
+	return 0;
+}
+
+// code made by ops400
