15c72289c72f45f39ef701a327e38357f5407dc0
OPS 400 <79289232+ops400@users.noreply.github.com>
2021-02-19 10:02:04 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "\033[31mred text\033[0m\n";
+
+	/* the \033 it's for start a chage on the text, the [ it's to declare the chages to be made like here the numbers chage the color of the text, the m it's for say to the c++ to display the text with the chages, now the [0m  it's to reset back to normal  */
+	
+	return 0;
+}
+
+// code made by ops400
