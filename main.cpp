a2a8734032e6a0079f72f022432be422d54087eb
OPS 400 <79289232+ops400@users.noreply.github.com>
2021-02-19 10:05:28 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	a = 1;
+	
+	while (a = 1)
+	{	
+		cout << "\033[41m \033[0m";
+		cout << "\033[42m \033[0m";
+		cout << "\033[43m \033[0m";
+		cout << "\033[44m \033[0m";
+		cout << "\033[45m \033[0m";
+		cout << "\033 [0m";
+	}
+
+	return 0;
+}
